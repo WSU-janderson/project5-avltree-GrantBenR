921aa05f429b16a85adf50fcb1f69a8baa8423ab
GrantBenR <thegrantrynders@gmail.com>
2025-11-05 22:24:13 -0500

    11_5 - Template functions and Node Class

---
+++ AVLNode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by test on 11/5/2025.
+//
+
+#include "AVLNode.h"
---
+++ AVLNode.h
@@ -0,0 +1,14 @@
+//
+// Created by test on 11/5/2025.
+//
+
+#ifndef AVLTREE_AVLNODE_H
+#define AVLTREE_AVLNODE_H
+
+
+class AVLNode
+{
+};
+
+
+#endif //AVLTREE_AVLNODE_H
